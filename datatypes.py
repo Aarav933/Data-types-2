f816d471a8667b1e6dc65b2f37b7f4445004e8c3
Aarav933 <techyusage@gmail.com>
2025-05-10 20:43:27 +0200

    Commit

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+name = "Bob"
+age = 13
+is_student= True
+weight = 40.5
+print("Name:", name)
+print("Data type of name is", type(name))
+print("Age:", age)
+print("Data type of name is", type(age))
+print("is_student:", is_student)
+print("Data type of name is", type(is_student))
+print("Weight:", weight)
+print("Data type of name is", type(weight))
